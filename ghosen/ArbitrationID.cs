681491d0defb30958d657137362c29e93dc65ef0
syntroniks <syntroniks@gmail.com>
2017-01-07 14:33:58 -0500

    Moved files

---
+++ ghosen/ArbitrationID.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ghosen
+{
+	/// <summary>
+	/// 11 bits long
+	/// </summary>
+	public struct ArbitrationId
+	{
+		public uint Id;
+
+		public ArbitrationId(uint _id) : this()
+		{
+			this.Id = _id;
+		}
+	}
+}
---
+++ ghosen/CandumpParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ghosen
+{
+	public class CandumpParser
+	{
+		// open file
+		// spin off worker
+		// {
+		// read file line
+		// parse file line
+		// return file line to thread safe queue
+		// }
+		// worker completed
+
+		public static async Task<List<CandumpLine>> ParseStream(Stream s)
+		{
+			var ret = new List<CandumpLine>();
+
+			var streamLength = s.Length;
+			using (StreamReader sr = new StreamReader(s))
+			{
+				while (!sr.EndOfStream)
+				{
+					var progress = ((double)sr.BaseStream.Position / (double)sr.BaseStream.Length) * 100.0d;
+					var currentLineStr = await sr.ReadLineAsync();
+					var currentLine = CandumpLine.Parse(currentLineStr);
+					Debug.WriteLine(currentLine);
+					ret.Add(currentLine);
+				}
+			}
+
+			return ret;
+		}
+	}
+}
