c7763e98fa70778e4adf74b97955882e3489dec1
syntroniks <syntroniks@gmail.com>
2017-01-07 19:50:52 -0500

    Done for today Now has prelim UDS service parsing

--- ghosen/MainWindow.xaml.cs
+++ ghosen/MainWindow.xaml.cs
@@ -33,6 +33,9 @@ namespace ghosen
 			var a = await CandumpParser.ParseLines(File.ReadAllLines("../../candump-2017-01-06_135802.log"), new ghosen.CandumpParserArbIdFilter(new List<uint>() { 0x7E0, 0x7E8 }));
 			sw.Stop();
 			Debug.WriteLine($"Parsed {a.Count} lines in {sw.ElapsedMilliseconds/1000.0d} seconds");
+			var messageList = a.Select((va) => va.Message).ToList();
+			var messages = ISO_TP.ISO_TP_Session.ProcessFrames(messageList);
+			var commands = ISO14229.Messages.ProcessMessages(messages);
 			//var p = CandumpParser.ParseStream(File.OpenRead("../../candump-2017-01-06_135802.log"));
 		}
 	}
