c01384d7aa41341f8c6bfc46b3abbae7ca05aca6
syntroniks <syntroniks@gmail.com>
2017-01-29 11:31:57 -0500

    Adds requestdownload and requestupload information Could add better negative response parsing in general.

--- ghosen/ISO14229/Messages.cs
+++ ghosen/ISO14229/Messages.cs
@@ -115,6 +115,89 @@ namespace ghosen.ISO14229
 			return $"{base.ToString()} : {SubFunction} {RoutineIdentifier} [{Utils.ByteArrayToHexViaLookup32(RoutineControlOptionRecord)}]";
 		}
 	}
+	public class RequestDownload : ISO14229Message
+	{
+		public enum CompressionMethod
+		{
+			None
+		}
+		public enum EncryptionMethod
+		{
+			None
+		}
+
+		// request  parameters
+		public CompressionMethod Compression { get; set; }
+		public EncryptionMethod Encryption { get; set; }
+
+		public int MemorySize { get; set; }
+		public uint MemoryAddress { get; set; }
+
+		// response parameters
+		public int MaxNumberOfBlockLengthLength { get; set; }
+		public int MaxNumberOfBlockLength { get; set; }
+
+		public RequestDownload(ISO_TP.Message message)
+			: base(message)
+		{
+			if (base.MessageType == ServiceMessageType.Request)
+			{
+				// High nibble
+				Compression = (CompressionMethod)((message.Payload[1] >> 4) & 0x0F);
+				// low nibble
+				Encryption = (EncryptionMethod)((message.Payload[1] >> 0) & 0x0F);
+
+				// High nibble
+				var memorySizeLength = (int)((message.Payload[2] >> 4) & 0x0F);
+				// low nibble
+				var memoryAddressLength = (int)((message.Payload[2] >> 0) & 0x0F);
+
+				// Memory address MSB first
+				for (int i = 0; i < memoryAddressLength; i++)
+				{
+					// Larger shifting because we process MSB first
+					var shiftAmount = 8 * (memoryAddressLength - i - 1);
+
+					// build memory address one byte at a time
+					MemoryAddress += (uint)message.Payload[3 + i] << shiftAmount;
+				}
+
+				// Same thing for memory size
+				for (int i = 0; i < memorySizeLength; i++)
+				{
+					// Larger shifting because we process MSB first
+					var shiftAmount = 8 * (memorySizeLength - i - 1);
+
+					// build memory size one byte at a time
+					MemorySize += message.Payload[memoryAddressLength + 3 + i] << shiftAmount;
+				}
+			}
+			else
+			{
+				MaxNumberOfBlockLengthLength = (int)((message.Payload[1] >> 4) & 0x0F);
+				for (int i = 0; i < MaxNumberOfBlockLengthLength; i++)
+				{
+					// Larger shifting because we process MSB first
+					var shiftAmount = 8 * (MaxNumberOfBlockLengthLength - i - 1);
+
+					// build memory size one byte at a time
+					MaxNumberOfBlockLength += message.Payload[2 + i] << shiftAmount;
+				}
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{base.ToString()} : 0x{MemoryAddress:X} : {MemorySize}";
+		}
+	}
+	public class RequestUpload : RequestDownload
+	{
+		public RequestUpload(ISO_TP.Message message)
+			: base(message)
+		{
+		}
+	}
 	public class ISO14229Message
 	{
 		public ServiceType Service { get; set; }
@@ -193,8 +276,10 @@ namespace ghosen.ISO14229
 					return new RoutineControl(message);
 					break;
 				case ServiceType.RequestDownload:
+					return new RequestDownload(message);
 					break;
 				case ServiceType.RequestUpload:
+					return new RequestUpload(message);
 					break;
 				case ServiceType.TransferData:
 					break;
