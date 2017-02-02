c7a4b96cbd9ea0b701f21e34a96fbfd6ad2802cb
ulabs-sgiroux <sag@ulabs.io>
2017-02-02 11:17:39 -0500

    Broke 14229 messages into their own files

---
+++ ghosen/ISO14229/Messages/DiagnosticSessionControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ghosen.ISO14229.Messages
+{
+    public class DiagnosticSessionControl : ISO14229Message
+    {
+        public enum DiagnosticSessionType
+        {
+            DefaultSession = 0x01,
+            ProgrammingSession = 0x02,
+            ExtendedDiagnosticSession = 0x03,
+            SafetySystemDiagnosticSession = 0x04
+        }
+
+        public DiagnosticSessionType SessionType { get; set; }
+
+        public DiagnosticSessionControl(ISO_TP.Message message)
+            : base(message)
+        {
+            SessionType = (DiagnosticSessionType)message.Payload[1];
+            if (base.MessageType == ServiceMessageType.Request)
+            {
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ ghosen/ISO14229/Messages/RoutineControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ghosen.ISO14229.Messages
+{
+    public class RoutineControl : ISO14229Message
+    {
+        public enum RoutineControlType
+        {
+            ISOSAEReserved,
+            StartRoutine,
+            StopRoutine,
+            RequestRoutineResults,
+        }
+
+        public RoutineControlType SubFunction { get; set; }
+
+        public UInt16 RoutineIdentifier { get; set; }
+
+        public byte[] RoutineControlOptionRecord { get; set; }
+
+        public RoutineControl(ISO_TP.Message message)
+            : base(message)
+        {
+            SubFunction = (RoutineControlType)message.Payload[1];
+            RoutineIdentifier = BitConverter.ToUInt16(message.Payload, 2);
+            RoutineControlOptionRecord = message.Payload.Skip(4).ToArray();
+            if (base.MessageType == ServiceMessageType.Request)
+            {
+            }
+            else
+            {
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} : {SubFunction} {RoutineIdentifier} [{Utils.ByteArrayToHexViaLookup32(RoutineControlOptionRecord)}]";
+        }
+    }
+}
